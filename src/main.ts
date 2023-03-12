f877212827f53ea5fc91e74e71bf06f53388302f
jvkass <jvcnmata@gmail.com>
2023-03-12 12:23:30 -0300

    feat: create swagger in structure base

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import * as compression from "compression";
+import * as bodyParser from "body-parser";
+import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  app.use(compression());
+  app.use(bodyParser.json({ limit: "5mb" }));
+  app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle("User API")
+    .setDescription("Open API with swagger")
+    .setVersion("1.0")
+    .addTag("user")
+    .addApiKey()
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableShutdownHooks();
+
+  const port = configService.getOrThrow<number>("PORT", 8001);
+
+  await app.listen(port);
 }
 bootstrap();
